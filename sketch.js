6003759699a104b3cd9dce792e5925f952be936f
Sofus287 <Sofus287@gmail.com>
2025-01-20 15:01:38 +0100

    Opdatering af sofus

--- sketch.js
+++ sketch.js
@@ -8,6 +8,10 @@ function setup() {
     .position(100, 100)
     .size(100, 70)
     .mousePressed(() => Dish());
+  let btn1 = createButton("Ingredients")
+  .position(200,100)
+  .size(100,70)
+  .mousePressed(()=> Ingredients());
   DDcategory = createSelect();
   DDcategory.position(0, 100);
   DDcategory.option("Miscellaneous");
@@ -20,16 +24,6 @@ function setup() {
   DDcategory.option("Seafood");
   DDcategory.option("Breakfast");
   DDcategory.option("Goat");
-  
-  sel = createSelect();
-  sel.position(10, 10);
-  sel.option('foo');
-  sel.option('bar');
-  sel.option('baz');
-  sel.style('background-color', 'orange');
-  sel.style('border-radius', '3px');
-  sel.style('width', '200px');
-  sel.style('padding', '0.5em');
   
 }
 
@@ -46,8 +40,23 @@ function Console(d) {
   textWrap(WORD);
 
   let randomMeal = random(d.meals);
+  let mealId = randomMeal.idMeal
 
   text(randomMeal.strMeal, 20, 200, 600);
   text(randomMeal.strCategory, 200, 200, 600);
   text(randomMeal.strInstructions, 20, 250, 600);
+}
+
+function Ingredients(){
+  let ingredientString="www.themealdb.com/api/json/v1/1/lookup.php?i=";
+
+  //loadJSON("www.themealdb.com/api/json/v1/1/lookup.php", Console1);
+  loadJSON(ingredientString + mealId(), Console1);
+}
+
+function Console1(e) {
+textWrap(WORD);
+
+let area=e.strArea
+text(area,50,200,200)
 }
